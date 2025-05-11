986fb45f3f976ac5084633a3f0955c7ed226c7bf
Caleb <136144800+cracker76239@users.noreply.github.com>
2025-05-11 12:12:34 -0500

    Updated tests

--- Tests/tests.py
+++ Tests/tests.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('C:/Users/caleb/Documents/GitHub/rhoPy')
 
 from random import *
-import pytest # type: ignore
+import pytest
 from rhoPy.rho import * # type: ignore
 
 # Some tests.
@@ -67,19 +67,15 @@ def testSamplingMeanStDev():
     assert dists.sampling.mean.stDev(10, 5) == pytest.approx(4.4721, rel=1e-2)
     assert dists.sampling.mean.stDev(2, 10) == pytest.approx(0.6325, rel=1e-2)
 
-def testBinomMean():
+def testBinom():
     assert dists.binom.mean(0.5, 10) == 5
     assert dists.binom.mean(0.3, 20) == 6
-
-def testBinomStDev():
     assert dists.binom.stDev(0.5, 10) == pytest.approx(1.58, rel=1e-2)
     assert dists.binom.stDev(0.2, 20) == pytest.approx(1.79, rel=1e-2)
 
-def testGeometMean():
+def testGeomet():
     assert dists.geomet.mean(0.5) == 2
     assert dists.geomet.mean(0.25) == 4
-
-def testGeometStDev():
     assert dists.geomet.stDev(0.5) == (0.5**0.5) / 0.5
     assert dists.geomet.stDev(0.25) == (0.75**0.5) / 0.25
 
@@ -92,11 +88,16 @@ def testMedian():
     assert median([1,2,3,4]) == 2.5
     
 def testQuickMedian():
-    assert quickMedian([1,2,3,4,5]) == 2.5
-    assert quickMedian([1,2,3,4]) == 3
+    assert quickMedian([1,2,3,4,5]) == 3
+    assert quickMedian([1,2,3,4]) == 2.5
     
 def testQuartiles():
     list = [1,2,3,4,5]
     assert Q1(list) == 1.5
     assert Q3(list) == 4.5
     assert IQR(list) == 3
+
+def test_chisq():
+    assert dstr.chisqrdist([[1,2,3],[4,5,6],[7,8,9]]) == [[1.6, 2.0, 2.4], [4.0, 5.0, 6.0], [6.4, 8.0, 9.6]]
+    assert dstr.chisqrdist([1,2,3],[2,3,4]) == [2,3,4]
+    assert dstr.chisqrdist([1,2,3]) == None
